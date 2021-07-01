1ca031f9a4cf4e1ce2547a1b3aaaf88c0a24e1d5
Evžen Wybitul <wybitul.evzen@gmail.com>
2021-07-01 09:58:48 +0200

    Change rendering script. Fixes #3, #4, and #5

---
+++ render.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p "rendered-pdf"
+mkdir -p "rendered-html"
+
+CSS_PATH="/Users/eugen/Library/Application Support/abnerworks.Typora/themes/vue.css"
+
+for f in lecture-*.md; do
+  html_filename="./rendered-html/${f%.*}.html"
+
+  echo "Converting $f to $html_filename"
+  pandoc --standalone --mathjax --css "$CSS_PATH" -o "$html_filename" -- "$f"
+
+  pdf_filename="./rendered-pdf/${f%.*}.pdf"
+  echo "Converting $f to $pdf_filename.pdf"
+  pandoc --katex --css "$CSS_PATH" -f markdown -t html5 -o "$pdf_filename" -- "$f"
+
+  sed -i "" "s|images/|../images/|g" "$html_filename"
+done
